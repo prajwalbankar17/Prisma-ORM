69607cfbc3bb13c579451f53194ca9dc99ec2644
prajwalbankar17 <prajwalbankar11@gmail.com>
2025-07-28 16:10:42 +0530

    to create multiple data at a single time

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-    // create (Insert Data)
-    // Single User
-    const user = await prisma.user.create({
-        data: {
-            name: "prajwal",
-            email: "prajwal@gmail.com",
-        }
-    })
-    console.log("User Created: ", user);
-}
+  // create (Insert Data)
+  // Single User
+//   const user = await prisma.user.create({
+//     data: {
+//       name: "prajwal",
+//       email: "prajwal@gmail.com",
+//     },
+//   });
+//   console.log("User Created: ", user);
+
+  //for multiple Users
+  const newUser = await prisma.user.createMany({
+    data: [
+      { name: "prajwal", email: "prajwal@gmail.com" },
+      { name: "Sai", email: "sai@gmail.com" },
+    ],
+  });
+  console.log("User Created: ", newUser);
+};
+
 makeStrictEnum()
   .catch((e) => console.log(e))
   .finally(async () => {
